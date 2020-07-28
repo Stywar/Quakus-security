180b9d855c55750d327156fb35fbf122e555aa0e
Stywar <franco111988@gmail.com>
2020-07-27 21:02:19 -0500

    se agrego codigo hiii

--- src/main/java/com/aforo255/security/ProfileResource.java
+++ src/main/java/com/aforo255/security/ProfileResource.java
@@ -23,7 +23,7 @@ public class ProfileResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello ! how are u ? hiiii Stywar";
+        return "hello ! how are u ? hiiii Stywar hh";
     }
     
     @Inject
